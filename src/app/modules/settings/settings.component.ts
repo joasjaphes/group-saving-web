33d121de14a33902608b1a295beab535dacf1e25
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2021-01-13 11:37:15 +0300

    adding basic layout for group settings

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -6,42 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-  // Basic Group Information
-  // Contribution Types
-  // Loan Types
-  // Leadership
-  // Meeting Rules
 
   menus = [
     {
       name: 'Basic Group Information',
       route: '',
       description: 'Update Basic group information like contribution frequency, currency, name and other policy',
-      image: ''
+      image: 'adjustment.png'
     },
     {
       name: 'Contribution Types',
       route: '',
       description: 'Set contribution types available for the group, Share, Social, Entry fees etc',
-      image: ''
+      image: 'money.png'
     },
     {
       name: 'Loan Types',
       route: '',
       description: 'Set loan configuration options, types of loan allowed, fines, profit contributions',
-      image: ''
+      image: 'request-money.png'
     },
     {
       name: 'Leadership',
       route: '',
       description: 'Select group leadership (Chairperson, Secretary and Treasure)',
-      image: ''
+      image: 'leadership1.png'
     },
     {
       name: 'Meeting Rules',
       route: '',
       description: 'Set rules for meetings to be help by the group, frequency, late attendance fines, missing meeting fines etc',
-      image: ''
+      image: 'meeting.png'
     },
   ];
   constructor() { }
