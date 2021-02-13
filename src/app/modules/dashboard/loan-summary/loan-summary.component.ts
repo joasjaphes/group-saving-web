00717182b3c6c9d2fe00f3908e9fabdf7141243e
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2021-02-13 06:53:35 +0300

    adding initial dashboard layout

--- src/app/modules/dashboard/loan-summary/loan-summary.component.ts
+++ src/app/modules/dashboard/loan-summary/loan-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Group} from '../../../store/group/group.model';
 
 @Component({
   selector: 'app-loan-summary',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanSummaryComponent implements OnInit {
 
+  @Input() group: Group;
   constructor() { }
 
   ngOnInit(): void {
