b0e3d2691f6687ba82ad9fcf4685ed84f6727aa9
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2021-03-13 12:42:28 +0300

    updating ngrx to v11

--- src/app/store/router/router.effect.ts
+++ src/app/store/router/router.effect.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 
-import {Effect, Actions, ofType} from '@ngrx/effects';
+import {createEffect, Actions, ofType} from '@ngrx/effects';
 import * as RouterActions from './router.action';
 
 import { tap, map } from 'rxjs/operators';
@@ -15,21 +15,21 @@ export class RouterEffects {
     private location: Location
   ) {}
 
-  @Effect({ dispatch: false })
-  navigate$ = this.actions$.pipe(ofType(RouterActions.GO),
+  
+  navigate$ = createEffect(() => this.actions$.pipe(ofType(RouterActions.GO),
     map((action: RouterActions.Go) => action.payload),
     tap(({ path, query: queryParams, extras }) => {
       this.router.navigate(path, { queryParams, ...extras });
     })
-  );
+  ), { dispatch: false });
 
-  @Effect({ dispatch: false })
-  navigateBack$ = this.actions$.pipe(
+  
+  navigateBack$ = createEffect(() => this.actions$.pipe(
     ofType(RouterActions.BACK),
-    tap(() => this.location.back()));
+    tap(() => this.location.back())), { dispatch: false });
 
-  @Effect({ dispatch: false })
-  navigateForward$ = this.actions$.pipe(
+  
+  navigateForward$ = createEffect(() => this.actions$.pipe(
     ofType(RouterActions.FORWARD),
-    tap(() => this.location.forward()));
+    tap(() => this.location.forward())), { dispatch: false });
 }
