7912de159d4fc2bfbcfb7a25add0b7716411c465
Vincent Minde <vincentminde@gmail.com>
2021-03-16 14:19:43 +0300

    Added testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  functionURL:'https://us-central1-group-saving.cloudfunctions.net/',
+  firebase: {
+    apiKey: 'AIzaSyCZlASesgqHoCE8YjsXhsJ7IO4-q3OWqlI',
+    authDomain: 'group-saving.firebaseapp.com',
+    databaseURL: 'https://group-saving.firebaseio.com',
+    projectId: 'group-saving',
+    storageBucket: 'group-saving.appspot.com',
+    messagingSenderId: '444964071176',
+    appId: '1:444964071176:web:732f9c6071c5a461b5eebd',
+    measurementId: 'G-JC2DKTVF1V'
+  }
 };
 
 /*
