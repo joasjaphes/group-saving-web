bcb0ea888a7cf4c46864012f543c9aa84f3d07a0
Joas Japhes <joasjaphes665@gmail.com>
2023-02-20 15:11:47 +0300

    added update password loginc

--- src/app/modules/members/update-member-password/update-member-password.component.ts
+++ src/app/modules/members/update-member-password/update-member-password.component.ts
@@ -1,11 +1,15 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CommonService } from 'src/app/services/common.service';
+import { FunctionsService } from 'src/app/services/functions.service';
+import { ApplicationState } from 'src/app/store';
 import { Group } from 'src/app/store/group/group.model';
 import { Member } from 'src/app/store/member/member.model';
 
 @Component({
   selector: 'app-update-member-password',
   templateUrl: './update-member-password.component.html',
-  styleUrls: ['./update-member-password.component.scss']
+  styleUrls: ['./update-member-password.component.scss'],
 })
 export class UpdateMemberPasswordComponent implements OnInit {
   @Input() group: Group;
@@ -16,15 +20,32 @@ export class UpdateMemberPasswordComponent implements OnInit {
 
   loading = false;
 
-  passWord:string;
-  confirmPassword:string;
-  constructor() { }
+  passWord: string;
+  confirmPassword: string;
+  constructor(
+    private commonService: CommonService,
+    private functionsService: FunctionsService,
+    private store: Store<ApplicationState>
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   closeDialog() {}
 
-  save() {}
-
+  async save() {
+    this.loading = true;
+    try {
+      const dataToSave = {
+        memberId: this.member.id,
+        phoneNumber: this.member.phone_number,
+        password: this.passWord,
+      };
+      await this.functionsService.saveData('updatePassword', dataToSave);
+      this.commonService.showSuccess('Members password updated successful');
+    } catch (e) {
+      console.error(e);
+      this.commonService.showError('Failed to update password');
+    }
+    this.loading = false;
+  }
 }
