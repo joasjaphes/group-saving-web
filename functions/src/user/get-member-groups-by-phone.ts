c36dc8524b46fb2d68acdbd008ba481409565847
Joas Japhes <joasjaphes665@gmail.com>
2023-03-11 19:24:04 +0300

    modified getUserGroupByPhone function

--- functions/src/user/get-member-groups-by-phone.ts
+++ functions/src/user/get-member-groups-by-phone.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as helpers from '../helpers';
 const cors = require('cors')({origin: true});
 
 export const getUserGroupsByPhoneNumber = functions.https.onRequest((request, response) => {
@@ -9,12 +10,25 @@ export const getUserGroupsByPhoneNumber = functions.https.onRequest((request, re
   }
   return cors(request, response, () => {
     const data = request.body;
+    let userGroups = [];
     console.log(data);
     console.log(request.get('Authorization'));
-    const fireStoreData = admin.firestore().collection('member_group').get();
-    fireStoreData.then(snapshot => response.send(snapshot.docs))
-    .catch(e => response.status(500).send('failed'));
-    response.status(200).send('Success');
+    const tokenId = request.get('Authorization')?.split('Bearer ')[1];
+    if ( tokenId !== helpers.token) {
+      response.status(400).send('Invalid token please send a valid token');
+      return;
+    }
+    admin.firestore().collection('member_group')
+    .where('phone_number', '==', data?.phoneNumber)
+    .get()
+    .then((querySnapshot) => {
+      userGroups = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+      response.status(200).send(userGroups);
+    })
+    .catch(e => {
+      console.log('Error fetching user data:', e);
+      response.status(500).send('Fail');
+    });
     // const tokenId = request.get('Authorization').split('Bearer ')[1];
     // if ( tokenId !== helpers.token) {
     //   response.status(400).send('Invalid token please send a valid token');
